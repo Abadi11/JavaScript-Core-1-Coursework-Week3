81cea98878378deabba72e9edbb7d20803dd0049
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-07-21 16:11:20 +0100

    1- exe H Done

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -7,7 +7,11 @@
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  let strToUpperCase = str.split("")[0].toUpperCase() + str.slice(1);
+  let newStr = strToUpperCase.split().join();
+  return newStr
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
