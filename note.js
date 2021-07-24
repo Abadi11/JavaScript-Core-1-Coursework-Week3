b4628838e404af193ea284c331249f1c5e52bda3
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-07-24 10:37:18 +0100

    Update note.js

--- note.js
+++ note.js
@@ -1,27 +1,30 @@
-function findSafeOxygenLevel(oxygen) {
-  let numberOfPercentage =[];
-  for (let i = 0; i<oxygen.length; i++){
-  oxygen[i].replace("%","");
-  let oxygenParseFloat = [];
-  //console.log(oxygenParseFloat)
-  oxygenParseFloat[i] = parseFloat(oxygen[i]);
-  //console.log(oxygenParseFloat[i])
-  numberOfPercentage.push(oxygenParseFloat[i])
-}
-function findOxygen (num){
-  if (num > 19.5 && num < 23.5){
-  return true;
-  }else{
-    return false;
+function sortAges(arr) {
+  let newArr = [];
+  //let newArr1 = [];
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] == Number ){
+     newArr.push(arr[i])
+    }
+    //newArr1.push(newArr[i])
   }
+  return newArr
 }
-let safeOxygen = numberOfPercentage.find(findOxygen);
-if (safeOxygen === undefined){
-  process.exit(1);
-}
-console.log(typeof safeOxygen)
-return safeOxygen.toString() + "%"
-}
-console.log(findSafeOxygenLevel(["50"]))
+const agesCase1 = [
+  "🎹",
+  100,
+  "💩",
+  55,
+  "🥵",
+  "🙈",
+  45,
+  "🍕",
+  "Sanyia",
+  66,
+  "James",
+  23,
+  "🎖",
+  "Ismeal",
+];
+console.log(sortAges(agesCase1))
 
 
